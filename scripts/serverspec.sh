020cd837e5973f8749aa8a55535a0724371e5e18
Kevin Green <Kevin.Green@HMCTS.NET>
2018-06-27 15:57:33 +0100

    proxy required for installing serverspec support modules

--- scripts/serverspec.sh
+++ scripts/serverspec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo gem install bundler --no-ri --no-rdoc
+sudo gem install bundler -p http://proxyout.reform.hmcts.net:8080/ --no-ri --no-rdoc
 cd /tmp/tests
 bundle install --path=vendor
 bundle exec rake spec
