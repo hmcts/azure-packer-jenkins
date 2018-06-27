7b73949d2cc1c40596d8e2848550d40be0f5bca6
Kevin Green <Kevin.Green@HMCTS.NET>
2018-06-27 16:57:44 +0100

    need the helper to load serverspec properly.

--- tests/spec/dynjenkins_user_spec.rb
+++ tests/spec/dynjenkins_user_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 describe user('dynjenkins') do
   it { should have_login_shell '/usr/bin/bash' }
 end
--- tests/spec/vault_token_spec.rb
+++ tests/spec/vault_token_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 describe file('/home/dynjenkins/.vault-token') do
   it { should be_file }
 end
