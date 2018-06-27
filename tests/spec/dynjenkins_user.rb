08d6c6a2cb9cbf20bc278fd608e26c051027cd6f
Kevin Green <Kevin.Green@HMCTS.NET>
2018-06-27 14:50:41 +0100

    add actual tests we want to perform.

---
+++ tests/spec/dynjenkins_user.rb
@@ -0,0 +1,3 @@
+describe user('dynjenkins') do
+  it { should have_login_shell '/usr/bin/bash' }
+end
---
+++ tests/spec/vault_token.rb
@@ -0,0 +1,3 @@
+describe file('/home/dynjenkins/.vault-token') do
+  it { should be_file }
+end
