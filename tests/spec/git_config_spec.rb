0419a0e22b1c1d4d28f6c7f10f7125298ad06bee
Kevin Green <Kevin.Green@HMCTS.NET>
2018-06-28 15:33:33 +0100

    remove git config check

--- tests/spec/git_config_spec.rb
+++
@@ -1,8 +0,0 @@
-## I'm not actually sure this is necessary because it may come from jenkins,
-## not from the CLI?
-
-require 'spec_helper'
-
-describe file('/home/dynjenkins/.gitconfig') do
-  it { should be_file }
-end
